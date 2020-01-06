24b827a012b261f21ba0196b66b149afc44a1121
Muhamad Amir <mmuhamadamirzaidi@gmail.com>
2020-01-06 19:03:41 +0800

    Load Data Into List View

--- app/src/main/java/com/mmuhamadamirzaidi/elkencarapptest/Adapter/CarListAdapter.java
+++ app/src/main/java/com/mmuhamadamirzaidi/elkencarapptest/Adapter/CarListAdapter.java
@@ -1,11 +1,17 @@
 package com.mmuhamadamirzaidi.elkencarapptest.Adapter;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.mmuhamadamirzaidi.elkencarapptest.Model.Car;
+import com.mmuhamadamirzaidi.elkencarapptest.R;
 
 import java.util.ArrayList;
 
@@ -36,8 +42,45 @@ public class CarListAdapter extends BaseAdapter {
         return position;
     }
 
+    public class ViewHolder{
+        ImageView item_car_image;
+
+        TextView item_car_manufacturer, item_car_name, item_car_price, item_car_plat_number;
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        View item = convertView;
+        ViewHolder holder = new ViewHolder();
+
+        if (item == null){
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            item = inflater.inflate(layout, null);
+
+            holder.item_car_image = item.findViewById(R.id.item_car_image);
+
+            holder.item_car_manufacturer = item.findViewById(R.id.item_car_manufacturer);
+            holder.item_car_name = item.findViewById(R.id.item_car_name);
+            holder.item_car_price = item.findViewById(R.id.item_car_price);
+            holder.item_car_plat_number = item.findViewById(R.id.item_car_plat_number);
+            item.setTag(holder);
+        }
+        else{
+            holder = (ViewHolder)item.getTag();
+        }
+
+        Car car = recordList.get(position);
+
+        holder.item_car_manufacturer.setText(car.getManufacturer());
+        holder.item_car_name.setText(car.getName());
+        holder.item_car_price.setText(car.getPrice());
+        holder.item_car_plat_number.setText(car.getPlat());
+
+        byte[] recordImage = car.getImage();
+        Bitmap bitmap = BitmapFactory.decodeByteArray(recordImage, 0, recordImage.length);
+        holder.item_car_image.setImageBitmap(bitmap);
+
+        return item;
     }
 }
--- app/src/main/java/com/mmuhamadamirzaidi/elkencarapptest/Modules/MainActivity.java
+++ app/src/main/java/com/mmuhamadamirzaidi/elkencarapptest/Modules/MainActivity.java
@@ -1,19 +1,29 @@
 package com.mmuhamadamirzaidi.elkencarapptest.Modules;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import com.mmuhamadamirzaidi.elkencarapptest.Adapter.CarListAdapter;
+import com.mmuhamadamirzaidi.elkencarapptest.Model.Car;
 import com.mmuhamadamirzaidi.elkencarapptest.R;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView main_icon_add;
 
     ListView listCar;
+
+    ArrayList<Car> cars;
+    CarListAdapter carListAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +34,10 @@ public class MainActivity extends AppCompatActivity {
 
         listCar = findViewById(R.id.listCar);
 
+        cars = new ArrayList<>();
+        carListAdapter = new CarListAdapter(this, R.layout.item_car, cars);
+        listCar.setAdapter(carListAdapter);
+
         main_icon_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -31,5 +45,37 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-    }
+
+        // Get all data from sqlite
+        Cursor cursor = AddCarDetailActivity.sqLiteHelper.getData("SELECT * FROM RECORD");
+        cars.clear();
+
+        while (cursor.moveToNext()){
+            int id = cursor.getInt(0);
+            String manufacturer = cursor.getString(1);
+            String name = cursor.getString(2);
+            String price = cursor.getString(3);
+            String plat = cursor.getString(4);
+
+            byte[] image = cursor.getBlob(5);
+
+            cars.add(new Car(id, manufacturer, name, price, plat, image));
+        }
+        carListAdapter.notifyDataSetChanged();
+
+        if (cars.size() == 0){
+            // No record, list view empty
+
+            Toast.makeText(this, "No record found!", Toast.LENGTH_SHORT).show();
+        }
+
+        listCar.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                // Set action when on item long click
+
+                return false;
+            }
+        });
+   }
 }
